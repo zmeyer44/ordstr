9546e89cd44dd15697b5ea5190595dfbe5a49fb1
zmeyer44 <zmmeyer44@gmail.com>
2023-10-05 23:58:21 -0400

    test rewrites for nip-05

--- next.config.js
+++ next.config.js
@@ -1,4 +1,13 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  async rewrites() {
+    return [
+      {
+        source: "/.well-known/nostr.json",
+        destination: "/api/well-known",
+      },
+    ];
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
