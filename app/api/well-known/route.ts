e21aa19539cb38b76c29daaa3fce7780d87e856f
zmeyer44 <zmmeyer44@gmail.com>
2023-10-06 00:15:15 -0400

    testing

--- app/api/well-known/route.ts
+++ app/api/well-known/route.ts
@@ -4,6 +4,7 @@ function handler() {
   return NextResponse.json({
     names: {
       zach: "17717ad4d20e2a425cda0a2195624a0a4a73c4f6975f16b1593fc87fa46f2d58",
+      _: "17717ad4d20e2a425cda0a2195624a0a4a73c4f6975f16b1593fc87fa46f2d58",
     },
   });
 }
