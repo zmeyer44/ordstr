04b5e3b6bc7baa9918c8e7b53469499ab9ce35cd
zmeyer44 <zmmeyer44@gmail.com>
2023-10-08 09:17:06 -0400

    testing changes

--- lib/hooks/useProfile.ts
+++ lib/hooks/useProfile.ts
@@ -2,6 +2,12 @@
 import { useProfile as useNostrProfile } from "nostr-react";
 import { nip19 } from "nostr-tools";
 export default function useProfile(pubkey: string) {
+  console.log("Testing", pubkey);
+  if (nip19.BECH32_REGEX.test(pubkey)) {
+    const decone = nip19.decode(pubkey);
+    console.log("decone", decone);
+  }
+  console.log("Testing", pubkey);
   const { data: userData, isLoading } = useNostrProfile({
     pubkey: nip19.BECH32_REGEX.test(pubkey)
       ? nip19.decode(pubkey).data.toString()
