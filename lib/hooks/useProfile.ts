61a123ea005dc4dc577cfeb62e8e9e68b808cbd5
zmeyer44 <zmmeyer44@gmail.com>
2023-10-08 09:14:06 -0400

    test processing

--- lib/hooks/useProfile.ts
+++ lib/hooks/useProfile.ts
@@ -1,9 +1,11 @@
 "use client";
 import { useProfile as useNostrProfile } from "nostr-react";
-
+import { nip19 } from "nostr-tools";
 export default function useProfile(pubkey: string) {
   const { data: userData, isLoading } = useNostrProfile({
-    pubkey,
+    pubkey: nip19.BECH32_REGEX.test(pubkey)
+      ? nip19.decode(pubkey).data.toString()
+      : pubkey,
   });
 
   return { user: userData, isLoading };
