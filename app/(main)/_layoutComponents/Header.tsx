ff60ec91b4dffe8c2e86cf02e00ed8cdb337e2c5
zmeyer44 <zmmeyer44@gmail.com>
2023-10-10 16:23:53 -0400

    sticky header

--- app/(main)/_layoutComponents/Header.tsx
+++ app/(main)/_layoutComponents/Header.tsx
@@ -4,15 +4,15 @@ import AccountButton from "./AccountButton";
 
 export default function Header() {
   return (
-    <div className="z-header sticky top-0 inset-x-0 flex flex-col items-stretch transition-all">
-      <div className="px-4 pt-5 md:pt-10 md:px-10">
-        <header className="mx-auto max-w-7xl overflow-hidden border-2 border-primary-foreground rounded-full bg-background-gray">
-          <div className="flex items-stretch h-[50px] md:h-[60px] divide-primary-foreground divide-solid divide-x-2">
+    <div className="sticky inset-x-0 top-0 z-header flex flex-col items-stretch transition-all">
+      <div className="px-4 pt-5 md:px-10 md:pt-10">
+        <header className="mx-auto max-w-7xl overflow-hidden rounded-full border-2 border-primary-foreground bg-background-gray">
+          <div className="flex h-[50px] items-stretch divide-x-2 divide-solid divide-primary-foreground md:h-[60px]">
             <Link
               href="/"
-              className="px-6 md:px-8 bg-accent/60 center hover:bg-accent/80"
+              className="center bg-accent/60 px-6 hover:bg-accent/80 md:px-8"
             >
-              <h1 className="text-primary-foreground uppercase font-bold">
+              <h1 className="font-bold uppercase text-primary-foreground">
                 Ordstr
               </h1>
             </Link>
--- app/(main)/layout.tsx
+++ app/(main)/layout.tsx
@@ -8,8 +8,12 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-    <div className="max-w-screen relative flex min-h-[100svh] w-full flex-col overflow-x-hidden">
-      <Header />
+    <div className="max-w-screen flex min-h-[100svh] w-full flex-col overflow-x-hidden ">
+      <div className="fixed inset-0 top-0 z-header">
+        <Header />
+      </div>
+      <div className="h-[74px] md:h-[104px]" />
+
       <main className="flex flex-1 flex-col">
         <div className="flex-1">{children}</div>
         <Footer />
